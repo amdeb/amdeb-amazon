8abed05368b91425314ed30047076c156a9fb8d9
yingliu4203 <yingliu4203@gmail.com>
2014-10-31 00:07:40 -0700

    import res_config in models __init__

--- amdeb_amazon/__init__.py
+++ amdeb_amazon/__init__.py
@@ -3,5 +3,4 @@
 # need to load models used by views and security files
 # use relative import path here because
 # Odoo add prefix to module name
-from .models import res_config
-import tests
+import models
