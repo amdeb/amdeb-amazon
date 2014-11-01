d5c9a8478da03a7e983b096039e9b97d1c95738d
yingliu4203 <yingliu4203@gmail.com>
2014-11-01 14:36:32 -0700

    fix logging format

--- amdeb_amazon/integrator/product_integrator.py
+++ amdeb_amazon/integrator/product_integrator.py
@@ -12,31 +12,23 @@ PRODUCT_TEMPLATE_MODEL_NAME = 'product.template'
 PRODUCT_PRODUCT_MODEL_NAME = 'product.product'
 
 
-@create_record_event(PRODUCT_TEMPLATE_MODEL_NAME)
-def create_product_template(id):
-    _logger.debug('create product template id: {}', id)
+@create_record_event(PRODUCT_PRODUCT_MODEL_NAME)
+def create_product(id):
+    _logger.debug('create product id: {}'.format(id))
 
 
 @write_record_event(PRODUCT_TEMPLATE_MODEL_NAME)
 def write_product_template(id, values):
-    _logger.debug('write product template id: {}, values: {}', id, values)
-
-
-@unlink_record_event(PRODUCT_TEMPLATE_MODEL_NAME)
-def unlink_product_template(id):
-    _logger.debug('unlink product template id: {}', id)
-
-
-@create_record_event(PRODUCT_PRODUCT_MODEL_NAME)
-def create_product(id):
-    _logger.debug('create product id: {}', id)
+    _logger.debug('write product template id: {}, values: {}'.format(
+        id, values)
+    )
 
 
 @write_record_event(PRODUCT_PRODUCT_MODEL_NAME)
 def write_product(id, values):
-    _logger.debug('write product id: {}, values: {}', id, values)
+    _logger.debug('write product id: {}, values: {}'.format(id, values))
 
 
 @unlink_record_event(PRODUCT_PRODUCT_MODEL_NAME)
 def unlink_product(id):
-    _logger.debug('unlink product id: {}', id)
+    _logger.debug('unlink product id: {}'.format(id))
