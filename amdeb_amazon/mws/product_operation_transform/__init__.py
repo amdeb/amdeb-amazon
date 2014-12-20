7f2f78a349b8c6c160b7f347a8f4648da9841d6a
yingliu4203 <yingliu4203@gmail.com>
2014-12-19 17:43:13 -0800

    fix import order in __init__.py

--- amdeb_amazon/mws/product_operation_transform/__init__.py
+++ amdeb_amazon/mws/product_operation_transform/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from .product_operation_transformer import ProductOperationTransformer
+# the order matters, import independent modules first
 from .product_unlink_tranformer import ProductUnlinkTransformer
 from .product_create_transformer import ProductCreateTransformer
 from .product_write_transformer import ProductWriteTransformer
+from .product_operation_transformer import ProductOperationTransformer
--- amdeb_amazon/mws/product_syncs/__init__.py
+++ amdeb_amazon/mws/product_syncs/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# the order matters: import independent modules first
 from .product_sync_new import ProductSyncNew
 from .product_sync_pending import ProductSyncPending
-from .product_sync_completed import ProductSyncCompleted
 from .product_creation_success import ProductCreationSuccess
 from .product_sync_chore import do_daily_chore
+from .product_sync_completed import ProductSyncCompleted
