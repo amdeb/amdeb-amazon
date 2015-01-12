3df0a91e5c51e0daeb69b421759825a917879316
yingliu4203 <yingliu4203@gmail.com>
2015-01-12 01:53:34 -0800

    fix parent sku in relation sync

--- amdeb_amazon/mws/product_sync_transform/relation_transfomer.py
+++ amdeb_amazon/mws/product_sync_transform/relation_transfomer.py
@@ -14,9 +14,11 @@ class RelationTransformer(BaseTransformer):
     """
     def _convert_sync(self, sync_op):
         sync_value = super(RelationTransformer, self)._convert_sync(sync_op)
-        template_id = sync_op[RECORD_ID_FIELD]
-        sync_value['ParentSKU'] = sync_op[PRODUCT_SKU_FIELD]
+        amazon_product = self._amazon_product.search_by_head(sync_op)
+        sync_value['ParentSKU'] = amazon_product[PRODUCT_SKU_FIELD]
+
         sync_value['Variants'] = []
+        template_id = sync_op[RECORD_ID_FIELD]
         created_variants = self._amazon_product.get_variants(template_id)
         has_variant = False
         for variant in created_variants:
